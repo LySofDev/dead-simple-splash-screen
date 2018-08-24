3ff23e4a89955255c3cc5b5a6ffe929a8d271c2b
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-08-24 03:53:37 -0400

    loading and errors react to HealthService.isUp resolution

--- src/splash-screen/splash-screen.component.spec.ts
+++ src/splash-screen/splash-screen.component.spec.ts
@@ -6,11 +6,24 @@ import { Observable, of } from 'rxjs';
 import { HealthService } from './health.service';
 import { SplashScreenComponent } from './splash-screen.component';
 
+// Mock class for the HealthService
 @Injectable()
 class MockHealthService {
-  isUp(url: string): Observable<boolean> {
-    return of(true);
+  isUpResult: boolean;
+
+  constructor(isUpResult: boolean) {
+    this.isUpResult = isUpResult;
   }
+
+  isUp(url: string): Observable<boolean> {
+    return of(this.isUpResult);
+  }
+
+}
+
+// Configuration for the TestBed
+interface TestBedConfiguration {
+  isUpResult: boolean;
 }
 
 describe('SplashScreenComponent', () => {
@@ -20,30 +33,86 @@ describe('SplashScreenComponent', () => {
   let component: SplashScreenComponent;
   let element: HTMLElement;
 
-  beforeEach(async () => {
+  function configureTestBed(config: TestBedConfiguration) {
+    // Configure the Test module
     TestBed
       .configureTestingModule({
         declarations: [
           SplashScreenComponent
         ],
         providers: [
-          { provide: HealthService, useClass: MockHealthService }
+          {
+            provide: HealthService,
+            useFactory: () => new MockHealthService(config.isUpResult)
+          }
         ]
       })
       .compileComponents();
+
+      // HealthService Mock and Spy setup
       healthService = TestBed.get(HealthService);
+      spyOn(healthService, 'isUp').and.callThrough();
+
+      // Component references
       fixture = TestBed.createComponent(SplashScreenComponent);
       component = fixture.componentInstance;
-      fixture.detectChanges();
-  });
+      element = fixture.debugElement.nativeElement;
+  }
 
   describe('when mounted', () => {
 
-    it('will call the isUp method on the HealthService', async(() => {
-      spyOn(healthService, 'isUp').and.callThrough();
-      component.ngOnInit();
+    beforeEach(async () => {
+      configureTestBed({ isUpResult: true });
+    });
+
+    it('should call the isUp method on the HealthService', async(() => {
+      fixture.detectChanges();
       expect(healthService.isUp).toHaveBeenCalled();
     }));
+
+    it('should have a true loading attribute', () => {
+      expect(component.loading).toBe(true);
+    });
+
+    it('should have an empty errors list', () => {
+      expect(component.errors.length).toEqual(0);
+    });
+
+  });
+
+  describe('when HealthService.isUp resolves to true', () => {
+
+    beforeEach(async () => {
+      configureTestBed({ isUpResult: true });
+    });
+
+    it('should have a false loading attribute', () => {
+      fixture.detectChanges();
+      expect(component.loading).toBe(false);
+    });
+
+    it('should have an empty errors list', () => {
+      fixture.detectChanges();
+      expect(component.errors.length).toEqual(0);
+    });
+
+  });
+
+  describe('when HealthService.isUp resolves to false', () => {
+
+    beforeEach(async () => {
+      configureTestBed({ isUpResult: false });
+    });
+
+    it('should have a false loading attribute', () => {
+      fixture.detectChanges();
+      expect(component.loading).toBe(false);
+    });
+
+    it('should not have an empty errors list', () => {
+      fixture.detectChanges();
+      expect(component.errors.length).not.toEqual(0);
+    });
 
   });
 
--- src/splash-screen/splash-screen.component.ts
+++ src/splash-screen/splash-screen.component.ts
@@ -7,13 +7,18 @@ import { HealthService } from './health.service';
   styleUrls: ['./splash-screen.component.scss']
 })
 export class SplashScreenComponent implements OnInit {
+  loading: boolean = true;
+  errors: string[] = [];
 
   constructor(
     private healthService: HealthService
   ) {}
 
   ngOnInit() {
-    this.healthService.isUp('');
+    this.healthService.isUp('').subscribe((up: boolean) => {
+      this.loading = false;
+      if (!up) this.errors.push("The service is not up.");
+    });
   }
 
 }
