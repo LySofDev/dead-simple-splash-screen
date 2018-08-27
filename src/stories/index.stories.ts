27f8b787c03a61469f138ea13286f6ed19a6091c
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-08-27 14:35:04 -0400

    Added  color testing stories

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -2,14 +2,43 @@ import { storiesOf } from '@storybook/angular';
 import { SplashScreenComponent, HealthService } from '../dead-simple-splash-screen';
 
 storiesOf('Splash Screen', module)
+
   .add('default', () => ({
     component: SplashScreenComponent,
-    // props: {
-    //   url: 'http://localhost:3000/status'
-    // },
-    // moduleMetadata: {
-    //   providers: [
-    //     HealthService
-    //   ]
-    // }
-  }));
+    props: {
+      endpoint: 'http://localhost:3000/status'
+    },
+    moduleMetadata: {
+      providers: [
+        { provide: HealthService, useValue: null }
+      ]
+    }
+  }))
+
+  .add('inverted', () => ({
+    component: SplashScreenComponent,
+    props: {
+      endpoint: 'http://localhost:3000/status',
+      textColor: "black",
+      backgroundColor: "white"
+    },
+    moduleMetadata: {
+      providers: [
+        { provide: HealthService, useValue: null }
+      ]
+    }
+  }))
+
+  .add('green on yellow', () => ({
+    component: SplashScreenComponent,
+    props: {
+      endpoint: 'http://localhost:3000/status',
+      textColor: "green",
+      backgroundColor: "yellow"
+    },
+    moduleMetadata: {
+      providers: [
+        { provide: HealthService, useValue: null }
+      ]
+    }
+  }))
